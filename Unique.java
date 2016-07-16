1c6d1c8baa0f3e2d8256891ba12b0611ec196ea6
VishaliAnbu <vishalipriya77@gmail.com>
2016-07-16 15:17:51 +0530

    Add files via upload

---
+++ Unique.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unique;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Unique {
+
+   
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int a[]=new int[10];
+       for(int i=0;i<a.length;i++)
+       {
+           a[i]=sc.nextInt();
+       }
+       for(int i=0;i<a.length;i++){
+           for(int j=i+1;j<a.length;j++){
+               if(a[i]==a[j])
+               {
+                   a[i]=-1;
+                   a[j]=-1;
+               }
+                   
+           }
+       }
+           
+       for(int i=0;i<a.length;i++)
+       {
+           if(a[i]!=-1)
+       
+       System.out.println(a[i]);  
+    }
+}}
