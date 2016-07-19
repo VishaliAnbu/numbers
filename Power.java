2670800ef41b9901563e02dbb97e93b4789ae7b6
VishaliAnbu <vishalipriya77@gmail.com>
2016-07-19 14:02:43 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package power;
+
+import java.util.Scanner;
+
+/**
+ 
+ * @author Student
+ */
+public class Power {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a = 7,b =1 ;
+        System.out.println(Math.pow(a,b));
+    }
+}
